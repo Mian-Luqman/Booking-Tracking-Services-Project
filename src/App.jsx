46cb4dbacd5ee2aaed05f1205d8b08c0e67ef7a0
Mian-Luqman <mianluqman09876@gmail.com>
2025-01-08 18:31:41 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <SignUp/>
-      {/* <Dashboard/> */}
-      {/* <BookingList/> */}
+{/*       <SignUp/> */}
+      <Dashboard/>
+      <BookingList/>
     </>
   )
 }
